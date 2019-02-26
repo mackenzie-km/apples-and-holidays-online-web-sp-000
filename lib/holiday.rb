064b8f9eeb825a1f83ca81779e38a6b39363bcb7
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-26 21:04:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -77,13 +77,13 @@ end
     supply.each do |supply|
        printing_supply += "#{supply}, "
 end
-
+end
 
 #print your string list with last comma cropped out
 crop = (printing_supply.length)-2
 puts "  #{string_holiday}: #{printing_supply[0...crop]}"
 
-end
+
 end
 
 def all_holidays_with_bbq(holiday_hash)
